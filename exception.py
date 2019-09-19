b3e6177227451d670e8544d10cf844c6685ecfbc
Christian Brown <chrisadbr@protonmail.com>
2019-09-19 20:40:17 +0300

    exception

---
+++ exception.py
@@ -0,0 +1,15 @@
+#prompt a user for a name
+name = input('Enter your name: ')
+try:
+    #prompt user for their age
+    age = int(input('Enter your age: '))
+
+    #checks user's age
+    if age > 20:
+        print(name.title() + ", you are old enough to drive now")
+    else:
+        print(name.title() + ", you are not allowed to drive")
+
+#Handle errors if occurs
+except ValueError:
+    print('Enter a valid number')
