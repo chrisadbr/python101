925a06502728faea12e9e3fd9126c4a1ee8e462b
Christian Brown <chrisadbr@protonmail.com>
2019-09-20 14:40:12 +0300

    Python variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Program that elaborate about variables
+
+#Assigning values to variables
+a, b, c = 12, 34, 90
+#testing values to find the greatest
+if a > b and a > c:
+    print(str(a) + " is the greatest")
+elif b > a and b > c:
+    print(str(b) + ", is the greatest")
+else:
+    print(str(c) + ", is the greatest")
+
