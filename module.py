f8e395e43947f1c07fb5c939262212e4bcc1e2fe
Christian Brown <chrisadbr@protonmail.com>
2019-09-25 21:27:06 +0300

    Module

--- module.py
+++ module.py
@@ -5,7 +5,7 @@ import math
 radius = int(input('Enter radius of a circle: '))
 
 # Calculate the area  of a circle
-area = round(math.pi * (radius ** 2), 2)
+area = round(math.pi * (math.pow(radius, 2)), 2)
 circumference = round(2 * (math.pi * radius), 2)
 
 # Print area and circumference of a circle
